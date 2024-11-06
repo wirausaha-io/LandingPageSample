d7b4cae74b531ec1bd57341b84c2c4b1e3f5cac0
wirausaha-io <manikyogantara@gmail.com>
2024-11-06 22:43:34 +0800

    Update catalogData.js

--- Script/catalogData.js
+++ Script/catalogData.js
@@ -5,37 +5,37 @@ const catalogData = [
       name: "Product",
       description: "Deskripsi singkat tentang produk 1.",
       price: 50000,
-      image: "/Assets/Images/6.png"
+      image: "../Assets/Images/6.png"
     },
     {
       name: "Product",
       description: "Deskripsi singkat tentang produk 2. Deskripsi singkat tentang produk 2. Deskripsi singkat tentang produk 2.",
       price: 75000,
-      image: "/Assets/Images/7.png"
+      image: "../Assets/Images/7.png"
     },
     {
       name: "Product",
       description: "Deskripsi singkat tentang produk 3.",
       price: 100000,
-      image: "/Assets/Images/8.png"
+      image: "../Assets/Images/8.png"
     },
     {
       name: "Product",
       description: "Deskripsi singkat tentang produk 2.",
       price: 75000,
-      image: "/Assets/Images/7.png"
+      image: "../Assets/Images/7.png"
     },
     {
       name: "Product",
       description: "Deskripsi singkat tentang produk 2.",
       price: 75000,
-      image: "/Assets/Images/7.png"
+      image: "../Assets/Images/7.png"
     },
     {
       name: "Product",
       description: "Deskripsi singkat tentang produk 1.",
       price: 50000,
-      image: "/Assets/Images/6.png"
+      image: "../Assets/Images/6.png"
     },
 
 
